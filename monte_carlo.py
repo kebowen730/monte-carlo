8a99de6030d6e76468b6fdba690877a6f8afc365
Kenneth Bowen <kbowen@ustudio.com>
2018-11-13 20:39:18 -0600

    expanding build_monte_carlo_array to return a numpy array and allow for monetary additions

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,6 +1,5 @@
 from numpy.random import normal
-from numpy import array
-import numpy
+import numpy as np
 
 import math
 
@@ -8,8 +7,15 @@ import math
 def monte_carlo_step(previous, rate, stdv):
     return previous * math.exp(normal(rate, stdv))
 
-def build_monte_carlo_array(previous, rate, stdv, depth):
+
+def build_monte_carlo_array(previous, rate, stdv, depth, addition=None):
     sim_series = [monte_carlo_step(previous, rate, stdv)]
+    if addition:
+        for i in range(depth-1):
+            sim_series.append(monte_carlo_step(sim_series[i]+addition[i], rate, stdv))
     for i in range(depth):
         sim_series.append(monte_carlo_step(sim_series[i], rate, stdv))
-    return array([sim_series])
+    return np.array([sim_series])
+
+
+def build_donations_array(start, rate, stdv, depth)
